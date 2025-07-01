408db0987ed815709481f1c2a66324514fc0467b
sanikaDeshmukh29 <sanikadesh29@gmail.com>
2025-07-01 18:54:48 +0530

    JPA integration
    Database connection and table creation by integrating Spring Data JPA and MySQL Driver Dependencies

--- src/main/java/com/sprk/student_management/entity/Student.java
+++ src/main/java/com/sprk/student_management/entity/Student.java
@@ -1,18 +1,34 @@
 package com.sprk.student_management.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Data
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int rollNo;
+
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(nullable = false)
     private int age;
+
+    @Column(nullable = false)
     private String gender;
+
+    @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false)
     private double percentage;
 
 
